2f30549a59a8bb2baad3ef2a525df41944058b37
Leslie <81410185+lesnolie@users.noreply.github.com>
2023-03-31 23:55:22 +0800

    Update expensive_expenses.js

--- expensive_expenses.js
+++ expensive_expenses.js
@@ -1,6 +1,7 @@
 const today = new Date();
-const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
-const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+const firstDayOfMonth = new Date(Date.UTC(today.getFullYear(), today.getMonth(), 1, 8));
+const lastDayOfMonth = new Date(Date.UTC(today.getFullYear(), today.getMonth() + 1, 1, 8));
+
 
 const { Client } = require('@notionhq/client');
 

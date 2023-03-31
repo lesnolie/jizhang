6c0b12cc2af44b380f385dfca2981dcfe16ef49f
Leslie <81410185+lesnolie@users.noreply.github.com>
2023-03-31 23:04:32 +0800

    Update totalPrice.js

--- totalPrice.js
+++ totalPrice.js
@@ -1,6 +1,10 @@
 const today = new Date();
 const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+firstDayOfMonth.setHours(0, 0, 0, 0);
+
 const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+lastDayOfMonth.setHours(0, 0, 0, 0);
+
 
 const { Client } = require('@notionhq/client');
 

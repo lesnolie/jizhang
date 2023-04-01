3bc6a7b49bcb7e3141c719c43ca0e1c0b42d3bd3
Leslie <81410185+lesnolie@users.noreply.github.com>
2023-04-01 18:43:40 +0800

    更新 totalPrice.js

--- totalPrice.js
+++ totalPrice.js
@@ -1,15 +1,3 @@
-const today = new Date();
-const timezoneOffset = -480; // 东八区为 UTC+8，即 -480 分钟
-
-const firstDayOfMonth = new Date(Date.UTC(today.getFullYear(), today.getMonth(), 1, 0, 0, 0) - timezoneOffset * 60 * 1000);
-const lastDayOfMonth = new Date(Date.UTC(today.getFullYear(), today.getMonth() + 1, 0, 0, 0, 0) - timezoneOffset * 60 * 1000);
-
-
-
-const { Client } = require('@notionhq/client');
-
-const databaseId = process.env.NOTION_DATABASE_ID;
-
 async function fetchAllPages(databaseId) {
   const notion = new Client({ auth: process.env.NOTION_API_KEY });
   let startCursor = undefined;
@@ -20,11 +8,21 @@ async function fetchAllPages(databaseId) {
       const requestOptions = {
         database_id: databaseId,
         filter: {
-          property: '时间',
-          date: {
-            on_or_after: firstDayOfMonth.toISOString(),
-            on_or_before: lastDayOfMonth.toISOString()
-          }
+          and: [
+            {
+              property: '时间',
+              date: {
+                on_or_after: firstDayOfMonth.toISOString(),
+                on_or_before: lastDayOfMonth.toISOString()
+              }
+            },
+            {
+              property: '价格',
+              number: {
+                greater_than: 0
+              }
+            }
+          ]
         }
       };
       if (startCursor) {
@@ -48,9 +46,9 @@ async function main() {
   console.log('Pages fetched:', pages);
 
   const totalPrice = pages.reduce((acc, page) => {
-    const priceProperty = Object.entries(page.properties).find(([key, value]) => key === "价格");
+    const priceProperty = page.properties['价格'];
     console.log("Price property:", priceProperty);
-    const price = priceProperty ? (priceProperty[1].number !== null ? priceProperty[1].number : 0) : 0;
+    const price = priceProperty ? priceProperty.number : 0;
     console.log('Page price:', price);
     return acc + price;
   }, 0);

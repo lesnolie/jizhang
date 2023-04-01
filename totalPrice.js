842604fc9280164bfcbc3d9953eef5b77528e93b
Leslie <81410185+lesnolie@users.noreply.github.com>
2023-04-01 20:49:43 +0800

    Update totalPrice.js

--- totalPrice.js
+++ totalPrice.js
@@ -1,41 +1,21 @@
 const today = new Date();
-const timezoneOffset = -480; // 东八区为 UTC+8，即 -480 分钟
-const year = today.getFullYear();
-const month = today.getMonth() + 1;
-const firstDayOfMonth = new Date(Date.UTC(year, month - 1, 1, 0, 0, 0) - timezoneOffset * 60 * 1000);
-const lastDayOfMonth = new Date(Date.UTC(year, month, 0, 0, 0, 0) - timezoneOffset * 60 * 1000);
+ const firstDayOfMonth = new Date(Date.UTC(today.getFullYear(), today.getMonth(), 1, 8));
 
 
-
-const { Client } = require('@notionhq/client');
-
+ const { Client } = require('@notionhq/client');
 const databaseId = process.env.NOTION_DATABASE_ID;
-
 async function fetchAllPages(databaseId) {
   const notion = new Client({ auth: process.env.NOTION_API_KEY });
   let startCursor = undefined;
   let allPages = [];
-
   try {
     while (true) {
       const requestOptions = {
         database_id: databaseId,
         filter: {
-          and: [
-            {
-              property: '时间',
-              date: {
-                on_or_after: firstDayOfMonth.toISOString(),
-                on_or_before: lastDayOfMonth.toISOString()
-              }
-            },
-            {
-              property: '价格',
-              number: {
-                greater_than: 0
-              }
-            }
-          ]
+          property: '时间',
+          date: {
+            on_or_after: firstDayOfMonth.toISOString()          }
         }
       };
       if (startCursor) {
@@ -53,20 +33,16 @@ async function fetchAllPages(databaseId) {
   }
   return allPages;
 }
-
 async function main() {
   const pages = await fetchAllPages(databaseId);
   console.log('Pages fetched:', pages);
-
   const totalPrice = pages.reduce((acc, page) => {
-    const priceProperty = page.properties['价格'];
+    const priceProperty = Object.entries(page.properties).find(([key, value]) => key === "价格");
     console.log("Price property:", priceProperty);
-    const price = priceProperty ? priceProperty.number : 0;
+    const price = priceProperty ? (priceProperty[1].number !== null ? priceProperty[1].number : 0) : 0;
     console.log('Page price:', price);
     return acc + price;
   }, 0);
-
   console.log('Total price:', totalPrice);
 }
-
 main();
